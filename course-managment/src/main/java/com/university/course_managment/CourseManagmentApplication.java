a1d7f51fa891b11a6746243b6eea075073985a9a
Weerawi <weerawikramatharindu@gmail.com>
2025-07-17 13:54:42 +0530

    first commit

---
+++ course-managment/src/main/java/com/university/course_managment/CourseManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.university.course_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagmentApplication.class, args);
+	}
+
+}
